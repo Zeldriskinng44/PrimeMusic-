7cd843230603da51a66f67872cf46f8ba15be285
Zeldriskinng44 <144282751+Zeldriskinng44@users.noreply.github.com>
2025-02-13 12:24:51 +0100

    Update config.js

--- config.js
+++ config.js
@@ -4,18 +4,18 @@ module.exports = {
   TOKEN: "",
   language: "en",
   ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "",
+  mongodbUri : "mongodb+srv://zonzin736:zin736@diso.wa1fx.mongodb.net/?retryWrites=true&w=majority",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
   activityName: "YouTube Music", 
   activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
-  SupportServer: "https://discord.gg/xQF9f9yUEM",
+  SupportServer: "https://discord.gg/8VaW2SvXB9",
   embedTimeout: 5, 
   errorLog: "", 
   nodes: [
      {
-      name: "Koi Node V4",
+      name: "INZEWORLD.COM (DE)",
       password: "saher.inzeworld.com",
       host: "lava.inzeworld.com",
       port: 3128,
