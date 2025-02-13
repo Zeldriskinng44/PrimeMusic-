7e02e5104fd0c353118dddf7b603ca913c5138c9
Zeldriskinng44 <144282751+Zeldriskinng44@users.noreply.github.com>
2025-02-13 12:51:54 +0100

    Update en.js

--- languages/en.js
+++ languages/en.js
@@ -1,5 +1,5 @@
 module.exports = {
-    footer: "Developed by SSRR | Prime Music v1.2",
+    footer: "Developed by zeldris Music",
     ping: {
       description: "Check the bot latency",
       response: "Pinging...",
