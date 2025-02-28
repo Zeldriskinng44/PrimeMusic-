5839c1e782bb61a354bf31e02e31de48ebb501b2
Zeldriskinng44 <144282751+Zeldriskinng44@users.noreply.github.com>
2025-02-28 00:42:08 +0000

    Update en.js

--- languages/en.js
+++ languages/en.js
@@ -8,7 +8,7 @@ module.exports = {
         responseTime: "- Bot Response Time : **{latency}ms**",
         websocketPing: "- WebSocket Ping : **{ping}ms**",
         uptime: "- Uptime : **{uptime}**",
-        footer: "Developed by zeldris Music"
+        footer: "Developed zeldris Music"
       }
     },
     addsong: {
